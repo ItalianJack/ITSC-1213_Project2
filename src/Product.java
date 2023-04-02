9dd80294c2b125d52c86f63fa7560561757c4d86
ItalianJack <92689827+ItalianJack@users.noreply.github.com>
2023-04-02 17:19:56 -0400

    continued

--- src/Product.java
+++ src/Product.java
@@ -22,8 +22,28 @@ public abstract class Product implements Comparable<Product> {
 
     @Override
     public int compareTo(Product arg0) {
-        // TODO Auto-generated method stub
-        return 0;
+        if (this.getClass().equals(arg0.getClass())) {
+            // If they are the same type, compare prices
+            if (this.getPrice() > arg0.getPrice()) {
+                return 1;
+            } else if (this.getPrice() < arg0.getPrice()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        } else if (this instanceof Book) {
+            // If the objects are different types and this is a Book, the other object can only be a CD or DVD, so return 1 because books come first
+            return 1;
+        } else if (this instanceof DVD) {
+            //If the objects are different types and this is a DVD, the other object can only be a Book or CD so return -1 because DVDs come last
+            return -1;
+        } else if (arg0 instanceof Book) {
+            //If the objects are different types, and this is neither a Book nor DVD, it must be a CD. If the other object is a book, return -1 because CDs come after books
+            return -1;
+        } else {
+            //Otherwise return 1 because the other object must be a DVD which come after CDs
+            return 1;
+        }
     }
 
 }
